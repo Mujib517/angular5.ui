ef54b7efdfbe9a14d0df8633899c59bfad8440de
Mujib517 <mujib517@gmail.com>
2018-03-22 09:28:54 +0530

    Pipe Transform Interface

--- src/app/shared/time.pipe.ts
+++ src/app/shared/time.pipe.ts
@@ -1,10 +1,10 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 import * as moment from 'moment';
 
 @Pipe({
     name: 'time'
 })
-export class TimePipe {
+export class TimePipe implements PipeTransform {
 
     transform(date: string): string {
         return moment(date).fromNow();
