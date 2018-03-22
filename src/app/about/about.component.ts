2ac3a829b41edabb31bbdec5400de916c6aafefa
Mujib517 <mujib517@gmail.com>
2018-03-22 08:40:06 +0530

    NgSwitch

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,47 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   template: `
+    <h1>About Page</h1>
 
+    <div [ngSwitch]="priority">
+      <h1 *ngSwitchCase="'High'">High Priority Task</h1>
+      <h1 *ngSwitchCase="'Medium'">Medium Priority Task</h1>
+      <h1 *ngSwitchCase="'Low'">Low Priority Task</h1>
+      <h1 *ngSwitchDefault>Very Low Priority Task</h1>
+    </div>
 
-    <h1 [ngClass]="cls">Heading</h1>
-
-    <h1 [ngStyle]="style">Heading2</h1>
-  
-  <div *ngIf="toggle">
-    ....
-    ...
-    .....
-    <h1  [class.fg]="myProperty">About Page</h1>
-  </div>
-
-  
   `
 })
 export class AboutComponent {
-  myProperty: string = "Hello Angular";
-  myProperty2: boolean;
-  style: any = {
-    'background-color': 'yellow',
-    border:'1px solid gray'
-  };
 
-  toggle: boolean = true;
-  cls: any = {
-    fg: true,
-    bg: this.myProperty
-  };
+  priority: string = "11";
 
-  //0,false,null,undefined,"",NaN
-
-  getValue() {
-    //complexx
-    return "Hello World";
-  }
-
-  onClick(param) {
-    if (this.toggle) this.toggle = false;
-    else this.toggle = true;
-  }
 }
 // 1.x ng-show ng-hide ngIf
-//directives ngModel,ngClass, ngStyle, ngIf, ngFor
+//directives ngModel,ngClass, ngStyle, ngIf, ngFor,ngSwitch
