8274c4b2fffcdbd2b8611a857e85bb5f76155524
Mujib517 <mujib517@gmail.com>
2018-03-26 09:13:10 +0530

    Retry operator

--- src/app/shared/book.service.ts
+++ src/app/shared/book.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retry';
 
 @Injectable()
 export class BookService {
@@ -10,6 +11,7 @@ export class BookService {
 
     get() {
         return this.http.get('https://node-rest-training.herokuapp.com/api/books')
+            .retry(3)
             .map(res => res["data"]);
     }
 }
