63754dd862c948de619a94775a1f92f637a4ceaa
Mujib517 <mujib517@gmail.com>
2018-03-29 08:58:25 +0530

    Validations

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,19 +9,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
    <div class="col-md-5">
         <div class="form-group">
           <input type="text" placeholder="Name" class="form-control" formControlName="name"/>
-          
+          <span *ngIf="frm.controls.name.touched && frm.controls.name.errors?.required" class="text-danger">Required</span>
+          <span *ngIf="frm.controls.name.touched && frm.controls.name.errors?.minlength" class="text-danger">Min 3 Chars</span>
+          <span *ngIf="frm.controls.name.touched && frm.controls.name.errors?.maxlength" class="text-danger">Max 10 Chars</span>
         </div>
         <div class="form-group">
           <input  type="text" placeholder="Author" class="form-control" formControlName="author" />
-          </div>
+          <span *ngIf="frm.controls.author.touched && frm.controls.author.errors?.required" class="text-danger">Required</span>  
+        </div>
         <div class="form-group">
           <input type="text" required="true" class="form-control" placeholder="Price" formControlName="price"/>
-        </div>
+          <span *ngIf="frm.controls.price.touched && frm.controls.price.errors?.required" class="text-danger">Required</span>
+          <span *ngIf="frm.controls.price.touched && frm.controls.price.errors?.pattern" class="text-danger">Invalid Price</span>
+          </div>
         <div class="form-group">
         InStock: <input type="checkbox" formControlName="inStock"/>
       </div>
       <div class="form-group">
-        <button class="btn btn-success" (click)="onSave()" >Save</button>
+        <button [disabled]="frm.invalid" class="btn btn-success" (click)="onSave()" >Save</button>
       </div>
       </div>
     </form>
@@ -33,9 +38,9 @@ export class ReactiveFormComponent {
 
   constructor(private fb: FormBuilder) {
     this.frm = this.fb.group({
-      name: [''],
-      author: [],
-      price: [''],
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+      author: ['', [Validators.required]],
+      price: ['', [Validators.required, Validators.pattern("[0-9]{3,5}")]],
       inStock: []
     });
   }
