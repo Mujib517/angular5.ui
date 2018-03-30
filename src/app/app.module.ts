3aafbe78abee2edb75c14206ef02bc7861ca02d1
Mujib517 <mujib517@gmail.com>
2018-03-30 09:24:20 +0530

    Barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from './about/about.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { BookComponent } from './book/book.component';
-import { BookService } from "./shared/book.service";
-import { ConsoleLogger } from "./shared/console.logger";
-import { FileLogger } from "./shared/file.logger";
-import { UsersComponent } from './users/users.component';
-import { HttpClientModule } from "@angular/common/http";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
-import { AppRouterModule } from "./shared/app.router.module";
 import { SharedModule } from "./shared/shared.module";
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { ReviewsComponent } from './reviews/reviews.component';
-import { SpecsComponent } from './specs/specs.component';
-import { NewBookComponent } from './new-book/new-book.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { AppRouterModule } from "./shared/app.router.module";
 
+import { AppComponent, HomeComponent, AboutComponent, BookListComponent, BookComponent, UsersComponent, HeaderComponent, FooterComponent, ContactComponent, BookDetailComponent, ReviewsComponent, SpecsComponent, NewBookComponent, ReactiveFormComponent }
+    from './app.barrel';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, ReactiveFormsModule, SharedModule, AppRouterModule],
