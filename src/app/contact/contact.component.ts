dabf0b3cc4e2b70438ddadccc183a7180088287b
Mujib517 <mujib517@gmail.com>
2018-04-04 08:58:33 +0530

    Change detection

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,6 +4,19 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-contact',
   template: `
     <h1>Contact Page</h1>
+
+    <h1>{{count}}</h1>
+
+    <button class="btn btn-sm" (click)="inc()">++</button>
   `
 })
-export class ContactComponent{}
+export class ContactComponent {
+
+  count: number = 0;
+
+  inc() {
+    this.count++;
+  }
+  //change detection
+  // asynchrounous operation
+}
